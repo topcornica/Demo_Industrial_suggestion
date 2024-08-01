70b692d368c89a6a0f7de1a0531fe12de9da993e
Nakarin Rujipunya <topcornica@Nakarins-MacBook-Air.local>
2024-08-01 11:00:21 +0700

    add demo app

---
+++ try_doc.py
@@ -0,0 +1,11 @@
+import docx
+
+def getText(filename):
+    doc = docx.Document(filename)
+    fullText = []
+    for para in doc.paragraphs:
+        fullText.append(para.text)
+    return '\n'.join(fullText)
+
+txt = getText('pump_data.docx')
+print)
